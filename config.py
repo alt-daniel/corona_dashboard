5181e880c9bbe2c7cc1e411024202edd4a80aea3
D.P.N.P. Altorf <daniel.altorf@hva.nl>
2020-12-14 13:24:58 +0100

    Setup project

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+SECRET_KEY = 'mdfjaggjh'
+FLASK_ENVIRONMENT = ''
+
+# DB credentials
+DB_USERNAME = ''
+DB_PASSWORD = ''
+DB_HOST = ''
+DB_SCHEMA = ''
+
+# Static
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(ROOT_DIR, "static")
